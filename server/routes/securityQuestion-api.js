e358a83ea2f67e11710ec12f27ed50dc00fb84b3
James Brown <james@Jamess-iMac.local>
2020-10-20 18:59:30 -0500

    adding find security question by id

--- server/routes/securityQuestion-api.js
+++ server/routes/securityQuestion-api.js
@@ -10,7 +10,7 @@
  */
 
 const express = require('express');
-//TODO Import Security Question model
+const SecurityQuestion = require('../models/securityQuestion');
 const router = express.Router();
 
 //bring in our base and error response classes
@@ -30,8 +30,28 @@ const ErrorResponse = require('../services/error-response');
  * --Find securityQuestion by ID--
  * 
  */
-
-
+router.get('/:_id', async(req, res) => {
+    try{
+        SecurityQuestion.findOne({
+            '_id': req.params._id
+        },
+        function(err, securityQuestion){
+            if(err){
+                console.log(err);
+                const findSecurityQuestionMongoDbError = new ErrorResponse('500', 'Internal Server Error!', err);
+                res.status(500).send(findSecurityQuestionMongoDbError.toObject());
+            } else {
+                console.log(securityQuestion);
+                const findSecurityQuestionMongoDbSuccess = new BaseResponse('200', 'Success!', securityQuestion);
+                res.json(findSecurityQuestionMongoDbSuccess.toObject());
+            }
+        })
+    } catch (e){
+        console.log(e);
+        const findSecurityQuestionCatchError = new ErrorResponse('500', 'Internal Server Error!', e.message);
+        res.status(500).send(findSecurityQuestionCatchError.toObject());
+    }
+})
 
 /**
 * 

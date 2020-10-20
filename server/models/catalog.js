86f0b67483195606bf554b9af44677aa7686c62c
bmulhern2 <bmulhern09@gmail.com>
2020-10-20 11:29:17 -0400

    code attribution on the catalog model folder

--- server/models/catalog.js
+++ server/models/catalog.js
@@ -1,3 +1,15 @@
+/**
+ * 
+ * ================================
+ * ; Title: BCRS PROJECT
+ * ; Authors: Sarah Kovar; James Brown; Brendan Mulhern
+ * ; Modified by: James Brown
+ * ; Date: 10/14/2020
+ * ; Description: Application for Bobs Computer Repair Shop.
+ * ================================
+ * 
+ */
+
 let mongoose = require('mongoose')
 
 let Schema = mongoose.Schema;

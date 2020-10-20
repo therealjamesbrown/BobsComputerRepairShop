3d286a6e070e55aaa5e412b0454e0d56a3702a6e
James Brown <james@Jamess-iMac.local>
2020-10-20 10:00:44 -0500

    udpating code attribution

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@
  * ================================
  * ; Title: BCRS PROJECT
  * ; Authors: Sarah Kovar; James Brown; Brendan Mulhern
- * ; Modified by: James Brown
  * ; Date: 10/14/2020
  * ; Description: Application for Bobs Computer Repair Shop.
  * ================================

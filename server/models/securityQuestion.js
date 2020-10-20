1d7dead62165dfb290b0ba0a29d35622ae5416a3
bmulhern2 <bmulhern09@gmail.com>
2020-10-20 14:15:45 -0400

    test

--- server/models/securityQuestion.js
+++ server/models/securityQuestion.js
@@ -1,3 +1,15 @@
+/**
+ * 
+ * ================================
+ * ; Title: BCRS PROJECT
+ * ; Authors: Sarah Kovar; James Brown; Brendan Mulhern
+ * ; Modified by: James Brown
+ * ; Date: 10/14/2020
+ * ; Description: Application for Bobs Computer Repair Shop.
+ * ================================
+ * 
+ */
+
 // Install Dependencies
 
 let mongoose = require('mongoose')
@@ -16,4 +28,5 @@ let SecurityQuestionSchema = new Schema({
     collection: "securityQuestions"
 })
 
+// Exports the securityQuestions model
 module.exports = mongoose.model('securityQuestions', SecurityQuestionSchema)

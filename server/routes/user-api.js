e0a9c54e19d07b5daf7824235cafa0f708161b35
James Brown <james@Jamess-iMac.local>
2020-10-22 10:47:06 -0500

    adding create user api

--- server/routes/user-api.js
+++ server/routes/user-api.js
@@ -12,10 +12,12 @@
 const express = require('express');
 const User = require('../models/user')
 const router = express.Router();
+var bcrypt = require('bcryptjs');
 
 //bring in our base and error response classes
 const BaseResponse = require('../services/base-response');
 const ErrorResponse = require('../services/error-response');
+const saltRounds = 10; //set the number of times the password is getting salted
 
 /**
  * 
@@ -45,8 +47,36 @@ const ErrorResponse = require('../services/error-response');
 * 
 */
 router.post('/', async(req, res) => {
-    
-})
+    try{
+        let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds); //salt that thang
+
+        let newUser = {
+            username:    req.body.username,
+            password:    hashedPassword,
+            firstName:   req.body.firstName,
+            lastName:    req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+            address:     req.body.address,
+            email:       req.body.email
+        }
+
+        User.create(newUser, function(err, user){
+            if(err){
+                console.log(err);
+                const createUserMongoDbErrorResponse = new ErrorResponse('500', 'Internal Server Error!', err);
+                res.status(500).send(createUserMongoDbErrorResponse.toObject());
+            } else {
+                console.log(user);
+                const createUserSuccessResponse = new BaseResponse('200', 'Success!', user);
+                res.json(createUserSuccessResponse.toObject());
+            }
+        })
+    } catch(e){
+        console.log(e);
+        const createUserCatchErrorResponse = new ErrorResponse('500', 'Internal Server Error', e.message);
+        res.status(500).send(createUserCatchErrorResponse.toObject());
+    }
+});
 
 
 /**

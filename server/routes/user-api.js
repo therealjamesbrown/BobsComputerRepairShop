5132ebe47ded3dde2070c7a26044550ef5f231d8
James Brown <james@Jamess-iMac.local>
2020-10-25 17:39:08 -0500

    adding role management progress

--- server/routes/user-api.js
+++ server/routes/user-api.js
@@ -14,7 +14,7 @@ const User = require('../models/user');
 const router = express.Router();
 var bcrypt = require('bcryptjs');
 let bodyParser = require('body-parser');
-let cors = require('cors')
+//let cors = require('cors') - 10/25 JB commenting out so Sarah and I can continue to work
 
 //bring in our base and error response classes
 const BaseResponse = require('../services/base-response');
@@ -22,7 +22,7 @@ const ErrorResponse = require('../services/error-response');
 //const { try } = require('bluebird'); //SK commented this out 10/22/20 at 10pm; no idea why this is here - I think it populates when you type 'try'
 const saltRounds = 10; //set the number of times the password is getting salted
 
-router.use(cors())
+//router.use(cors()) - //let cors = require('cors') - 10/25 JB commenting out so Sarah and I can continue to work
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: false }));
 /**
--- src/app/pages/administration/rolemanagement/rolemanagement.component.ts
+++ src/app/pages/administration/rolemanagement/rolemanagement.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { RoleService } from 'src/app/pages/administration/services/role.service';
+import { Role } from '../interfaces/role.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { HttpClient } from '@angular/common/http';
+import {MatPaginator} from '@angular/material/paginator';
+//todo create a taskDialogComponent
+
 
 @Component({
   selector: 'app-rolemanagement',
   templateUrl: './rolemanagement.component.html',
   styleUrls: ['./rolemanagement.component.css']
 })
-export class RolemanagementComponent implements OnInit {
 
-  constructor() { 
+
+
+
+export class RolemanagementComponent implements OnInit {
+  
+
+  
+roles: Role[];
+displayedColumns: string[] = ['role', 'status', 'action'];
+actions: string[] = ['update', 'disable'];
+
+  constructor(private http: HttpClient, private dialog: MatDialog, private roleServce: RoleService) { 
     /**
       * Find All Roles
       */
+     this.roleServce.findAllRoles().subscribe(res => {
+       this.roles = res['data'];
+       console.log(this.roles);
+     }, err => {
+       console.log(err);
+     })
+
+     
+    
   }
 
   ngOnInit(): void {
@@ -22,5 +50,6 @@ export class RolemanagementComponent implements OnInit {
    * SECTION FOR ROLE API (create, update, delete)
    * You'll need to import http client and the related service
    */
+ 
 
 }

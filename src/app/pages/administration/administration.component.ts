5de6e3f6d0272cd29dadfaa54f8b940b9e664757
bmulhern2 <bmulhern09@gmail.com>
2020-10-27 10:59:16 -0400

    moved user-admin

--- src/app/pages/administration/administration.component.ts
+++ src/app/pages/administration/administration.component.ts
@@ -11,20 +11,6 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { MatDialog } from '@angular/material/dialog'
-import { PostDialogComponent } from '../administration/dialogs/user-admin-post-dialog/post-dialog.component'
-import { PutDialogComponent } from '../administration/dialogs/user-admin-put-dialog/put-dialog.component'
-import { PatchDialogComponent } from '../administration/dialogs/user-admin-patch-dialog/patch-dialog.component'
-import { RoleService } from './services/role.service';
-import { CookieService } from 'ngx-cookie-service';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-  })
-}
 
 @Component({
   selector: 'app-administration',
@@ -39,34 +25,6 @@ export class AdministrationComponent implements OnInit {
   users: any
 
 
-  constructor(public dialog: MatDialog, private http: HttpClient, private roleService: RoleService, private cookieService: CookieService) { 
-
-   
-  }
-
-
-  ngOnInit() {
-    //Begin Brendans code
-    this.http.get('http://localhost:3000/api/users', httpOptions).subscribe(data => {
-      this.users = data['data'];
-    })
-    //end Brendans code
-   }
-
-   /**Begin Brendans code */
-   post() {
-     this.dialog.open(PostDialogComponent)
-   }
-   update(user) {
-    this.cookieService.set('updateId', user._id)
-    this.dialog.open(PutDialogComponent)
-   }
-   delete(user) {
-    this.cookieService.set('deleteId', user._id)
-    this.dialog.open(PatchDialogComponent)
-   }
-   ///**End Brendans code */
-
-
-  
+  constructor() { }
+  ngOnInit() { }   
 }
--- src/app/pages/administration/usermanagement/usermanagement.component.ts
+++ src/app/pages/administration/usermanagement/usermanagement.component.ts
@@ -1,28 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { MatDialog } from '@angular/material/dialog'
+import { PostDialogComponent } from '../dialogs/user-admin-post-dialog/post-dialog.component'
+import { PutDialogComponent } from '../dialogs/user-admin-put-dialog/put-dialog.component'
+import { PatchDialogComponent } from '../dialogs/user-admin-patch-dialog/patch-dialog.component'
+import { RoleService } from '../services/role.service';
+import { CookieService } from 'ngx-cookie-service';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+}
 @Component({
   selector: 'app-usermanagement',
   templateUrl: './usermanagement.component.html',
   styleUrls: ['./usermanagement.component.css']
 })
-export class UsermanagementComponent implements OnInit {
+export class UserManagementComponent implements OnInit {
+  
+  //Variable Declaration
+  users: any
 
-  constructor() { 
-     /**
-     * Find all Users
-     */
 
+  constructor(public dialog: MatDialog, private http: HttpClient, private roleService: RoleService, private cookieService: CookieService) { }
+
+
+  ngOnInit() {
+    //Begin Brendans code
+    this.http.get('http://localhost:3000/api/users', httpOptions).subscribe(data => {
+      this.users = data['data'];
+    })
+    //end Brendans code
+   }
+
+   /**Begin Brendans code */
+   post() {
+     this.dialog.open(PostDialogComponent)
+   }
+   update(user) {
+    this.cookieService.set('updateId', user._id)
+    this.dialog.open(PutDialogComponent)
+   }
+   delete(user) {
+    this.cookieService.set('deleteId', user._id)
+    this.dialog.open(PatchDialogComponent)
+   }
   }
-
-  ngOnInit(): void {
-  }
-
-  /**
-    * 
-    * SECITION FOR USER MANAGEMENT API CALLS (create, update, delete)
-    * You'll need to import http client and the related service
-    * 
-    */
-
-
-}
